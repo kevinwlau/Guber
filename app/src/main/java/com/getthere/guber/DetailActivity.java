183cc6398b5f6862ac68f7e5064c5e7eb17b9552
Manav <manavm@utexas.edu>
2014-12-08 14:52:43 -0600

    Added new transition page

--- app/src/main/java/com/getthere/guber/DetailActivity.java
+++ app/src/main/java/com/getthere/guber/DetailActivity.java
@@ -1,9 +1,9 @@
 package com.getthere.guber;
 
 import android.content.Intent;
-import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -57,6 +57,7 @@ public class DetailActivity extends ActionBarActivity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                 Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
+
             Intent intent = getActivity().getIntent();
 
             if (intent != null && intent.hasExtra(Intent.EXTRA_TEXT)) {
@@ -64,6 +65,7 @@ public class DetailActivity extends ActionBarActivity {
                 ((TextView) rootView.findViewById(R.id.detail_text))
                         .setText(forecastStr);
             }
+
 
             return rootView;
         }
